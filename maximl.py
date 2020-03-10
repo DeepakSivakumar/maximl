6627c36840f5b504c2cada26665daa2ff11fe5e7
DeepakSivakumar <49530610+DeepakSivakumar@users.noreply.github.com>
2020-03-10 22:25:39 +0530

    Update maximl.py

--- maximl.py
+++ maximl.py
@@ -1,8 +1,17 @@
 string=input()
-list=[]
-count=0
-for i in string:
-    if i not in list:
-        list.append(i)
-        count=count+1
-print(count)
+def distinct(s):
+    i, j = 0, 0
+    I, J = 0, 0
+    Empty_set = set([])
+    while j < len(s):
+        if s[j] in Empty_set:
+            Empty_set.remove(s[i])
+            i += 1
+        else:
+            Empty_set.add(s[j])
+            j += 1
+        if J - I < j - i:
+            I, J = i, j
+    return s[I:J]
+print(len(distinct(string)))
+    
